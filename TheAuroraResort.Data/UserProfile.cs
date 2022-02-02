f373a06fe699f44d18a059f5304b8de10de3ea14
Savannah Jackson <savannahjackson33@gmail.com>
2022-02-01 19:49:17 -0500

    Add project files.

---
+++ TheAuroraResort.Data/UserProfile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheAuroraResort.Data
+{
+    public class UserProfile
+    {
+        public string UserEmail { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public IList<string> Reservations { get; set; }
+
+        [Key]
+        public int ProfileId { get; set; }
+
+        public bool IsMember { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
---
+++ TheAuroraResort/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheAuroraResort.Startup))]
+namespace TheAuroraResort
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
