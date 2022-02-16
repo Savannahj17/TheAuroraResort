37ed4063d82510a30222edc8e8c536c742c77d98
Savannah Jackson <savannahjackson33@gmail.com>
2022-02-16 11:00:41 -0500

    ReservationController

--- TheAuroraResort/Controllers/ReservationController.cs
+++ TheAuroraResort/Controllers/ReservationController.cs
@@ -69,8 +69,50 @@ namespace TheAuroraResort.Controllers
             var detail = service.GetReservationById(ReservationId);
             var model = new ReservationEdit
             {
+                ReservationId = detail.ReservationId,
+                PartySize = detail.PartySize,
+                ReservationDateTime = detail.ReservationDateTime
+            };
 
-            }
+            return View(model);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(int ReservationId, ReservationEdit model)
+        {
+            if (ModelState.IsValid)
+                return View(model);
+
+            if(model.ReservationId != ReservationId)
+            {
+                ModelState.AddModelError("", "These Id's do not match.");
+                return View(model);
+            }
+
+            var service = CreateReservationService();
+
+            if (service.UpdateReservation(model))
+            {
+                TempData["SaveResult"] = "Your reservation has been updated!";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Your reservation could not be updated at this time. Please try again later or try calling 1-800-555-5555. We apologize for the inconvenience.");
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(int ReservationId)
+        {
+            var service = CreateReservationService();
+            service.DeleteReservation(ReservationId);
+            TempData["SaveResult"] = "Your reservation has been successfully deleted.";
+            return RedirectToAction("Index");
+        }
+
     }
 }
