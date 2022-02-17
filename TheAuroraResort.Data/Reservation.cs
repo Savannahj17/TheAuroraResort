b7e2ef843c0c13dc77f9611fb99f7d8e3e48531a
Savannah Jackson <savannahjackson33@gmail.com>
2022-02-16 19:59:22 -0500

    Commit

--- TheAuroraResort.Data/Reservation.cs
+++ TheAuroraResort.Data/Reservation.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace TheAuroraResort.Data
 {
@@ -21,7 +22,38 @@ namespace TheAuroraResort.Data
 
         [Required]
         public string UserEmail { get; set; }
-        public DateTime ReservationDateTime { get; set; }
+        public DateTimePicker ReservationDateTime { get; set; }
+
+        public void SetDateTimePickerFormat()
+        {
+            ReservationDateTime.Format = DateTimePickerFormat.Custom;
+            ReservationDateTime.CustomFormat = "MMMM dd, yyyy - t:mm tt";
+        }
+
+        private void InitializeDateTimePicker()
+        {
+            // Construct the DateTimePicker.
+            this.ReservationDateTime = new System.Windows.Forms.DateTimePicker();
+
+            //Set size and location.
+            this.ReservationDateTime.Location = new System.Drawing.Point(40, 88);
+            this.ReservationDateTime.Size = new System.Drawing.Size(160, 21);
+
+            // Set the alignment of the drop-down MonthCalendar to right.
+            this.ReservationDateTime.DropDownAlign = LeftRightAlignment.Right;
+
+            // Set the Value property to 50 years before today.
+            ReservationDateTime.Value = System.DateTime.Now.AddYears(-50);
+
+            //Set a custom format containing the string "of the year"
+            ReservationDateTime.Format = DateTimePickerFormat.Custom;
+            ReservationDateTime.CustomFormat = "MMM dd, 'of the year' yyyy ";
+
+            // Add the DateTimePicker to the form.
+            Forms.Controls.Add(this.ReservationDateTime);
+        }
+
+
 
         //DateTimePicker//
         //edit models
@@ -39,5 +71,7 @@ namespace TheAuroraResort.Data
         public DateTimeOffset? ModifiedUtc { get; set; }
 
     }
+
+    
 }
 
